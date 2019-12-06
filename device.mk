707b52726c15c224c5c2ac276b6b7b3790d1d99a
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    Use 6GB Dalvik heap configuration
    We are currently using 2GB Dalvik heap configuration
    on a 6GB device.
    
    Bug: 145823510
    Test: MPTS
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- device.mk
+++ device.mk
@@ -740,8 +740,8 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.sys.ssr.restart_level=modem,slpi,adsp
 
-# setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+# Setup Dalvik VM configurations
+$(call inherit-product, vendor/extras/configs/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Use the default charger mode images
 PRODUCT_PACKAGES += \
